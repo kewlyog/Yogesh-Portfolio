b58963a9e59f10510e683e510f4a1a9920f6031c
Your Name <your.email@example.com>
2025-06-18 18:29:49 +0530

    Update layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yog - Portfolio",
+  title: "Yogesh's - Portfolio",
   description: "Full Stack Developer & Software Engineer Portfolio",
 };
 
